28230b71892e05ccacf39dbb89f3a5c5a53e39bd
Ethan Hall <ethan@ehdev.io>
2015-10-11 14:11:58 -0700

    Added configuration and test for the config

--- model-api/src/main/java/io/ehdev/conrad/backend/config/VersionBackendConfiguration.java
+++ model-api/src/main/java/io/ehdev/conrad/backend/config/VersionBackendConfiguration.java
@@ -1,9 +1,9 @@
-package io.ehdev.conrad.backend.commit;
+package io.ehdev.conrad.backend.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("io.ehdev.conrad.backend.commit")
-public class CommitModelConfiguration {
+@ComponentScan("io.ehdev.conrad.backend.version")
+public class VersionBackendConfiguration {
 }
