9f5f5ee366a3031a158589185a7bc54478a6e5a1
Ethan Hall <ethan@ehdev.io>
2016-01-11 19:39:20 -0800

    Made logger better

--- web-service/webapp/src/main/java/io/ehdev/conrad/app/config/MetricsConfiguration.java
+++ web-service/webapp/src/main/java/io/ehdev/conrad/app/config/MetricsConfiguration.java
@@ -1,12 +1,19 @@
 package io.ehdev.conrad.app.config;
 
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Slf4jReporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 public class MetricsConfiguration {
+
+    private static final Logger logger = LoggerFactory.getLogger(MetricsConfiguration.class);
 
     @Bean
     @ConditionalOnMissingBean(MetricRegistry.class)
@@ -14,14 +21,16 @@ public class MetricsConfiguration {
         return new MetricRegistry();
     }
 
-//    @Bean
-//    @ConditionalOnMissingBean(ConsoleReporter.class)
-//    public ConsoleReporter consoleReporter(MetricRegistry metricRegistry) {
-//        ConsoleReporter reporter = ConsoleReporter.forRegistry(metricRegistry)
-//            .convertRatesTo(TimeUnit.SECONDS)
-//            .convertDurationsTo(TimeUnit.MILLISECONDS)
-//            .build();
-//        reporter.start(1, TimeUnit.MINUTES);
-//        return reporter;
-//    }
+    @Bean
+    @ConditionalOnMissingBean(Slf4jReporter.class)
+    public Slf4jReporter consoleReporter(MetricRegistry metricRegistry) {
+        Slf4jReporter reporter = Slf4jReporter.forRegistry(metricRegistry)
+            .convertRatesTo(TimeUnit.SECONDS)
+            .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .outputTo(logger)
+            .withLoggingLevel(Slf4jReporter.LoggingLevel.INFO)
+            .build();
+        reporter.start(1, TimeUnit.MINUTES);
+        return reporter;
+    }
 }
