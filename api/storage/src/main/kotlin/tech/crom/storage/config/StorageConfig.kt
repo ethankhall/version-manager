c7f75d7e4e85a1ce525f12bf53c9de00ef1995a4
Ethan Hall <ethan@ehdev.io>
2016-10-04 20:21:46 -0700

    Allow default storage in google cloud

--- api/storage/src/main/kotlin/tech/crom/storage/config/StorageConfig.kt
+++ api/storage/src/main/kotlin/tech/crom/storage/config/StorageConfig.kt
@@ -1,8 +1,16 @@
 package tech.crom.storage.config
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @ComponentScan("tech.crom.storage")
-open class StorageConfig
+open class StorageConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = arrayOf("storage.default.credentials"), havingValue = "google-cloud")
+    open fun googleCredentials(): GoogleCredential = GoogleCredential.getApplicationDefault()
+}
